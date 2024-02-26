6ae1dda9472c2ecc10aa03294e498cb512b584e8
Clown-Box <Sm3n26@gmail.com>
2024-02-26 11:06:03 -0500

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by 6f on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
